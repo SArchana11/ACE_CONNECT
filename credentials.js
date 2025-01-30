7465b3899e2bbd6503791bbab74042ba8144ab9a
SArchana11 <archanasamala2005@gmail.com>
2025-01-30 21:49:31 +0530

    Add files via upload

---
+++ credentials.js
@@ -0,0 +1,16 @@
+export const credentials = [
+  {
+    email: "satwiksharma1202@gmail.com",
+    password: "satwik@2005",
+  },
+  {
+    email:"archanasamala@gmail.com",
+    password:"archana@2005",
+  },
+  {
+    email:"rahul@gmail.com",
+    password:"rahul@2003",
+  }
+];
+
+export default credentials;
